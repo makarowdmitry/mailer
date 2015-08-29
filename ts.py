18449b7101bcfce5533887563a75d1376d5af2f5
Dmitry <makarow.dmitry@gmail.com>
2015-08-30 00:12:32 +0400

    added exception

--- ts.py
+++ ts.py
@@ -12,7 +12,7 @@ import time
 import logging
 
 BEARS = MailerBears() #Initial class MailerBears()	
-RECIPIENT = BEARS.get_recipient('data/myemail2.txt') #List recipient	
+RECIPIENT = BEARS.get_recipient('data/myemail4.txt') #List recipient	
 PATH_SOCKS = 'http://109.234.38.38/media/sss/o4.txt' #List Socks
 SOCKS = BEARS.get_socks(PATH_SOCKS)
 # SOCKS = ['46.101.224.82,3128,SOCKS5,goemailgo,q8uir']
@@ -21,8 +21,12 @@ TO = 1
 BCC = 15 #random.randint(10,16)
 CC = 0
 
+LOCK = threading.RLock() #Initial Lock	
+
 LOGGING_FILE = 'data/mylog.log'
 logging.basicConfig(format = '%(levelname)-3s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = LOGGING_FILE)
+
+IPS = {}
 
 # State = namedtuple('State', 'addr ok fail')
 
@@ -52,6 +56,12 @@ class Mailerhi():
 		except:
 			return False
 
+	def record_log_and_counter(self,stat_sent,ip):
+		# Принимает данные после отправки пакетов. Пишет в лог. И записывает в общий счетчик статусов отправки(ok,spam и прочее) по этому соксу
+		# Возвращает True
+		logging.info(ip+' '+stat_sent)
+		return True
+
 	def sent_email(self,server,ehlo,recipient,bcc):
 		# Generate letter, generate fromaddress, activate_socks,отправка.
 		# Возврат результатов данной отправки
@@ -61,8 +71,8 @@ class Mailerhi():
 		
 		try:	
 
-			result = server.sendmail(fromaddr,recipient,letter)
-			print 'Success'
+			server.sendmail(fromaddr,recipient,letter)
+			result = 'Success'
 
 		except socks.ProxyConnectionError as error:
 			result = 'ProxyConnectionError:'+str(error)
@@ -85,8 +95,6 @@ class Mailerhi():
 			print result
 
 		except smtplib.SMTPDataError as error:
-			result1='DataError: '+str(error[0])+' '+error[1][:55]+'...'
-			print result1
 			
 			#421 Ratelimit exceeded for 103.253.146.218. Try again later.
 			if error[0]==421 and error[1].find('Ratelimit exceeded')!=-1:
@@ -96,24 +104,29 @@ class Mailerhi():
 				print result
 
 			#421 Temporarily rejected for 46.101.11.242. Try again later.
-			if error[0]==421 and error[1].find('Temporarily rejected')!=-1:
+			elif error[0]==421 and error[1].find('Temporarily rejected')!=-1:
 				regex = re.compile(r'for\s+\b([\d]{1,3}[.][\d]{1,3}[.][\d]{1,3}[.][\d]{1,3})\b')
 				temporarily_rejected = regex.search(error[1])
 				result = temporarily_rejected.group()
 				print result
 
 			#421 Problem resolving DNS for domain jmmon.com (DNS query timed out)
-			if error[0]==421 and error[1].find('DNS')!=-1:
+			elif error[0]==421 and error[1].find('DNS')!=-1:
 				regex = re.compile(r'domain\s+\b([\w\d.]+[.][\w\d.]+)\b')
 				problem_resolving = regex.search(error[1])
-				print problem_resolving.group()	
+				result = problem_resolving.group()
+				print result
 
 			#550 spam message rejected. Please visit
 			#550
-			if error[0]==550 and error[1].find('invalid mailbox')!=-1:
+			elif error[0]==550 and error[1].find('invalid mailbox')!=-1:
 				regex = re.compile(r'\b([\w.]+@[\w.]+[.][\w]+)\b')
 				invalid_mailbox = regex.search(error[1])
 				result = invalid_mailbox.group()
+				print result
+
+			else:
+				result='DataError: '+str(error[0])+' '+error[1][:55]+'...'
 				print result
 
 		except smtplib.SMTPConnectError as error:
@@ -129,37 +142,43 @@ class Mailerhi():
 			print result
 
 		except BaseException as error:
-			print error
-		return result
+			result = error
+			print result
 
-	# def record_log_and_counter(self,stat_sent):
-	# 	# Принимает данные после отправки пакетов. Пишет в лог. И записывает в общий счетчик статусов отправки(ok,spam и прочее) по этому соксу
-	# 	# Возвращает True
-	# 	pass
+
+
+		return result
 
 	# def record_recipient_ok_fail(self,fail_recipient,ok_recipient,bounce_recipient):
 	# 	# Принимает три списка с получателями, дописывает в списки. 
 	# 	# Если ок не пустой - в список success_sent.txt. Если bounce не пустой - в список bounce.txt. Если fail не пустой - дописываем в список RECIPIENT
 	# 	pass
 
-	def start_sent(self,ehlo,count):
+	def start_sent(self,socks_active,count):
 		# Получаем получаетелей
 		# Отправляем письмо
 		# Записываем статистику
 		# Записываем все успешные и неуспешно отправленные адреса и отскоки
 		# with self._lock:
 		# 	ehlo = BEARS.socks_activate(socks)
+		# with self._lock:
+			# print socks_active[0]
+		ehlo = socks_active['hostname'][0]
 		print ehlo
+		ip = socks_active['ip']
+		print ip
 		try:
 			server_mail = 'mxs.mail.ru'
 			server = smtplib.SMTP(server_mail)
 			server.ehlo(ehlo)
-			i=1
-			while i!=0:
+			i=100
+			while i!=1:
 				with self._lock:
 					recipients = self.recipient_get()
 					i=len(RECIPIENT)
 				stat_sent = self.sent_email(server,ehlo,recipients[1],recipients[0])
+				with self._lock:
+					self.record_log_and_counter(stat_sent,ip)
 			server.quit()
 
 		except:
@@ -169,10 +188,15 @@ class Mailerhi():
 def threading_socks(socks,concurrent):
 	with futures.ThreadPoolExecutor(max_workers=concurrent) as pool:
 		mailer = Mailerhi(pool)
-		ehlo = BEARS.socks_activate(socks)
+
+		LOCK.acquire()
+		socks_active = BEARS.socks_activate(socks)
 		
-		for _ in pool.map(mailer.start_sent,ehlo,range(concurrent)):
-			pass		
+		
+		for _ in pool.map(mailer.start_sent,BEARS.socks_activate(socks),range(concurrent)):
+			print socks_active
+			pass	
+		LOCK.release()	
 
 	return True
 
@@ -180,6 +204,11 @@ def threading_socks(socks,concurrent):
 if __name__ == '__main__':
 	# Проверяем по stat, когда останавливать рассылку
 	# for _ in range(3):
+
+
 	for socks in SOCKS:
 		thread_ = threading.Thread(target=threading_socks,args=(socks,CONCURRENT))
 		thread_.start()
+
+	# m = Mailerhi()
+	# m.record_log_and_counter('wegweg')
